79114d6fe6987c62dda41c0ff222be04299487b3
talbn1 <talbn1@gmail.com>
2020-06-07 10:40:09 +0300

    first commit

---
+++ src/main/java/com/talbn1/beer_microservices/BeerMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.talbn1.beer_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talbn1/beer_microservices/BeerMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talbn1.beer_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
